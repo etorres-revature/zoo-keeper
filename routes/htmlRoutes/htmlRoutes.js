bc0809a8482c913800dbcfe9eb76e3fbab6859f9
Eric D. Torres <etorresnotary@gmail.com>
2021-10-01 00:44:14 -0500

    getAnimals front end working

--- routes/htmlRoutes/htmlRoutes.js
+++ routes/htmlRoutes/htmlRoutes.js
@@ -5,4 +5,16 @@ router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
+router.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/animals.html"));
+});
+
+router.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/zookeepers.html"));
+});
+
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
+});
+
 module.exports = router;
