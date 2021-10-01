20cea38badf830fbebf9a0f2fe209fc169fcfc99
Eric D. Torres <etorresnotary@gmail.com>
2021-09-30 22:58:49 -0500

    personality traits filter

--- utils/filter.js
+++ utils/filter.js
@@ -1,5 +1,22 @@
 const filterByQuery = (query, animalsArray) => {
+  let personalityTraitsArray = [];
+
   let filteredResults = animalsArray;
+
+  if (query.personalityTraits) {
+    if (typeof query.personalityTraits === "string") {
+      personalityTraitsArray = [query.personalityTraits];
+    } else {
+      personalityTraitsArray = query.personalityTraits;
+    }
+  }
+
+  personalityTraitsArray.forEach((trait) => {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.personalityTraits.indexOf(trait) !== -1
+    );
+  });
+
   if (query.diet) {
     filteredResults = filteredResults.filter(
       (animal) => animal.diet === query.diet
@@ -7,11 +24,15 @@ const filterByQuery = (query, animalsArray) => {
   }
 
   if (query.species) {
-    filteredResults.filter((animal) => animal.species === query.species);
+    filteredResults = filteredResults.filter(
+      (animal) => animal.species === query.species
+    );
   }
 
   if (query.name) {
-    filteredResults.filter((animal) => animal.name === query.name);
+    filteredResults = filteredResults.filter(
+      (animal) => animal.name === query.name
+    );
   }
 
   return filteredResults;
