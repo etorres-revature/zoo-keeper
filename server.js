7084b21c21903ce427427c2b6603640ce11154c6
Eric D. Torres <etorresnotary@gmail.com>
2021-09-30 22:44:22 -0500

    res json animals

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const { animals } = require("./db/animals.json");
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+app.get("/api/animals", (req, res) => {
+  res.json(animals);
+});
 
 app.listen(PORT, () =>
   console.log(`Server is live at http://localhost:${PORT}`)
